49444eec8b2d7046ebfe632dbd8f1bfcea5f7390
Vlad Zat <vlad.zat@mydit.ie>
2017-10-18 22:01:57 +0100

    Contour Testing

--- testDifferences.py
+++ testDifferences.py
@@ -20,7 +20,10 @@ pcb2 = cv2.imread(imagesPath + 'pcb2.jpg')
 
 img1Size = pcb1.shape
 img2Size = pcb2.shape
-print img1Size, img2Size
+
+def getDistance(x1, y1, x2, y2):
+    # return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
+    return ((x1 - x2)**2 + (y1 - y2)**2)**(1/2.0)
 
 def getMatches(img1, img2):
     akaze = cv2.AKAZE_create()
@@ -53,15 +56,53 @@ def getMatches(img1, img2):
     return matchedCoordinates
 
 matches = getMatches(pcb1, pcb2)
-for match in matches:
-    pt1 = match['pt1']
-    pt2 = match['pt2']
-    radius = 5
-    cv2.circle(pcb1, (int(pt1['x']), int(pt1['y'])), radius, (0, 0, 255), -1)
-    cv2.circle(pcb2, (int(pt2['x']), int(pt2['y'])), radius, (0, 0, 255), -1)
-    # pcb1[pt1['y'], pt1['x']] = (0, 0, 255)
-    # pcb2[pt2['y'], pt2['x']] = (0, 0, 255)
 
-cv2.imshow('pcb1', pcb1)
+# minDist = img1Size[0]
+# noMatches = len(matches)
+# for index1 in range(0, noMatches - 1):
+#     for index2 in range(index1 + 1, noMatches):
+#         coord1 = matches[index1]['pt1']
+#         coord2 = matches[index2]['pt1']
+#         currentDistance = getDistance(coord1['x'], coord1['y'], coord2['x'], coord2['y'])
+#         if currentDistance < minDist:
+#             minDist = currentDistance
+# print minDist
+
+
+# for match in matches:
+#     pt1 = match['pt1']
+#     pt2 = match['pt2']
+#     radius = 5
+#     cv2.circle(pcb1, (int(pt1['x']), int(pt1['y'])), radius, (0, 0, 255), -1)
+#     cv2.circle(pcb2, (int(pt2['x']), int(pt2['y'])), radius, (0, 0, 255), -1)
+
+mask = np.zeros((img1Size[0], img1Size[1], 1), np.uint8)
+mask[:, :] = 0
+for match in matches:
+    pt2 = match['pt2']
+    mask[int(pt2['y']), int(pt2['x'])] = 255
+    # radius = 1
+    # cv2.circle(mask, (int(pt1['x']), int(pt1['y'])), radius, (255, 255, 255), -1)
+
+shape = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, shape)
+# mask = cv2.bitwise_not(mask)
+
+_, contours, _ = cv2.findContours(image = mask.copy(), mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_SIMPLE)
+
+# cv2.drawContours(pcb2, contours, -1, (0, 0, 255), 3)
+
+# TODO Invert colours so the differences are detected instead of the same things
+for contour in contours:
+    arcPercentage = 0.1
+    epsilon = cv2.arcLength(curve = contour, closed = True) * arcPercentage
+    corners = cv2.approxPolyDP(curve = contour, epsilon = epsilon, closed = True)
+    x, y, w, h = cv2.boundingRect(points = corners)
+    currentArea = w * h
+
+    cv2.rectangle(pcb2, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+cv2.imshow('mask', mask)
+# cv2.imshow('pcb1', pcb1)
 cv2.imshow('pcb2', pcb2)
 cv2.waitKey(0)
